cd9506bcc7060b503e8c8c7efb04e8a2ccc3ef6f
Serg <sergio.pintaldi@gmail.com>
2019-11-29 14:19:48 +1100

    remove header from attribute and pass it as paramenters in functions, change default values, add radius and fovs attributes

--- pipeline/image/main.py
+++ pipeline/image/main.py
@@ -31,40 +31,41 @@ class FitsImage(Image):
         super().__init__(path)
 
         # set other attributes
-        self.header = self.__get_header(hdu_index)
-        self.polarisation = self.header.get('STOKES', 'I')
+        header = self.__get_header(hdu_index)
+        self.polarisation = header.get('STOKES', 'I')
 
         # set the rest of the attributes
-        self.__set_data_for_telescope()
+        self.__set_data_for_telescope(header)
 
         # get the frequency
-        self.__get_frequency()
+        self.__get_frequency(header)
 
     def __get_header(self, hdu_index):
         with fits.open(self.path) as hdulist:
             hdu = hdulist[hdu_index]
         return hdu.header.copy()
 
-    def __set_data_for_telescope(self):
+    def __set_data_for_telescope(self, header):
         '''
         set the image attributes depending on the telescope type
         '''
         self.datetime = pd.Timestamp('1900-01-01T00:00:00')
-        self.beam_bmaj = None
-        self.beam_bmin = None
-        self.beam_bpa = None
+        self.beam_bmaj = 0.
+        self.beam_bmin = 0.
+        self.beam_bpa = 0.
         self.ra = None
         self.dec = None
         self.fov_bmaj = None
         self.fov_bmin = None
 
-        if self.header.get('TELESCOP', None) == 'ASKAP':
-            self.datetime = pd.Timestamp(self.header['DATE'])
-            self.beam_bmaj = self.header['BMAJ']
-            self.beam_bmin = self.header['BMIN']
-            self.beam_bpa = self.header['BPA']
+        if header.get('TELESCOP', None) == 'ASKAP':
+            self.datetime = pd.Timestamp(header['DATE'])
+            self.beam_bmaj = header['BMAJ']
+            self.beam_bmin = header['BMIN']
+            self.beam_bpa = header['BPA']
 
             params = {
+                'header': header,
                 'fits_naxis1': 'NAXIS1',
                 'fits_naxis2': 'NAXIS2',
                 'fits_cdelt1': 'CDELT1',
@@ -74,10 +75,13 @@ class FitsImage(Image):
             # set the coordinate attributes
             self.__get_coordinates(**params)
 
-    def __get_coordinates(self, fits_naxis1, fits_naxis2, fits_cdelt1, fits_cdelt2):
-        wcs = WCS(self.header, naxis=2)
-        x = self.header[fits_naxis1]
-        y = self.header[fits_naxis2]
+        # get the time as Julian Datetime using Pandas function
+        self.datetime_jd = self.datetime.to_julian_date()
+
+    def __get_coordinates(self, header, fits_naxis1, fits_naxis2, fits_cdelt1, fits_cdelt2):
+        wcs = WCS(header, naxis=2)
+        x = header[fits_naxis1]
+        y = header[fits_naxis2]
         pix_centre = [[x / 2., y / 2.], [x / 2., y / 2.]]
         # sky_centre = wcs.wcs_pix2sky(pix_centre, 1)
         sky_centre = wcs.wcs_pix2world(pix_centre, 1)
@@ -89,24 +93,28 @@ class FitsImage(Image):
         # We leave a pixel margin at the edge that we don't use.
         # TODO: move unused pixel as argument
         unusedpix = 0.
-        usable_radius_pix = self.__get_radius_pixels(fits_naxis1, fits_naxis2) - unusedpix
-        self.fov_bmin = usable_radius_pix * abs(self.header[fits_cdelt1])
-        self.fov_bmaj = usable_radius_pix * abs(self.header[fits_cdelt2])
+        usable_radius_pix = self.__get_radius_pixels(header, fits_naxis1, fits_naxis2) - unusedpix
+        self.fov_bmin = usable_radius_pix * abs(header[fits_cdelt1])
+        self.fov_bmaj = usable_radius_pix * abs(header[fits_cdelt2])
 
-    def __get_radius_pixels(self, fits_naxis1, fits_naxis2):
+        # set the pixels radius
+        # TODO: check calcs
+        self.radius_pixels = usable_radius_pix
+
+    def __get_radius_pixels(self, header, fits_naxis1, fits_naxis2):
         """
         Return the radius (pixels) of the full image.
         If the image is not a square/circle then the shortest radius will be returned.
         """
         if self.entire_image:
             #a large circle that *should* include the whole image (and then some)
-            diameter = np.hypot(self.header[fits_naxis1], self.header[fits_naxis2])
+            diameter = np.hypot(header[fits_naxis1], header[fits_naxis2])
         else:
             # We simply place the largest circle we can in the centre.
-            diameter = min(self.header[fits_naxis1], self.header[fits_naxis2])
+            diameter = min(header[fits_naxis1], header[fits_naxis2])
         return diameter / 2.
 
-    def __get_frequency(self):
+    def __get_frequency(self, header):
         """
         Set some 'shortcut' variables for access to the frequency parameters
         in the FITS file header.
@@ -117,7 +125,6 @@ class FitsImage(Image):
         self.freq_eff = None
         self.freq_bw = None
         try:
-            header = self.header
             if header['TELESCOP'] in ('LOFAR', 'AARTFAAC'):
                 self.freq_eff = header['RESTFRQ']
                 if 'RESTBW' in header:
