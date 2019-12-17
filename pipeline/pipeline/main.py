f3ffc1e6765ba290a60444fca6adff09360d89e9
Serg <sergio.pintaldi@gmail.com>
2019-12-17 13:47:06 +1100

    added generating association, added skipping image processing if already present in DB, fix names of columns, creating catalogs objects in DB, creating association objects in DB

--- pipeline/pipeline/main.py
+++ pipeline/pipeline/main.py
@@ -7,7 +7,7 @@ from astropy import units as u
 from astropy.coordinates import Angle, match_coordinates_sky, SkyCoord
 
 from ..image.main import SelavyImage
-from ..models import Band, Catalog, Image, Source, SurveySource
+from ..models import Association, Band, Catalog, Image, Source, SurveySource
 from ..utils.utils import deg2hms, deg2dms
 
 
@@ -22,10 +22,14 @@ def get_source_models(row):
     return src
 
 
-def get_catalog_models(row):
+def get_catalog_models(row, dataset=None):
     name = f"catalog_{deg2hms(row['ave_ra'])}{deg2dms(row['ave_dec'])}"
-    c = Catalog(name=name, ave_ra=row['ave_ra'], ave_dec=row['ave_dec'])
-    return c
+    return Catalog(
+        name=name,
+        ave_ra=row['ave_ra'],
+        ave_dec=row['ave_dec'],
+        dataset=dataset
+    )
 
 
 class Pipeline():
@@ -42,7 +46,7 @@ class Pipeline():
             self.max_img=self.config.MAX_BACKWARDS_MONITOR_IMAGES + 1
 
         # A dictionary of path to Fits images, eg "/data/images/I1233234.FITS" and
-        # selavy catalogues
+        # selavy catalogs
         # Used as a cache to avoid reloading cubes all the time.
         self.image_paths = {
             x:y for x,y in zip(config.IMAGE_FILES, config.SELAVY_FILES)
@@ -50,6 +54,7 @@ class Pipeline():
 
     def process_pipeline(self, dataset):
         images = []
+        src_dj_obj = pd.DataFrame()
         for path in self.image_paths:
             # STEP #1: Load image and sources
             image = SelavyImage(path, self.image_paths[path])
@@ -59,45 +64,59 @@ class Pipeline():
             band_id = self.get_create_img_band(image)
 
             # 1.2 create image entry in DB
-            img = self.get_create_img(dataset, band_id, image)
+            img, exists_f = self.get_create_img(dataset, band_id, image)
             # add image to list
             images.append(img)
+            if exists_f:
+                logger.info(f'image {img.name} already processed, grab sources')
+                # grab the sources and skip to process next image
+                sources = (
+                    pd.Series(
+                        Source.objects.filter(image__id=img.id),
+                        name='src_dj'
+                    )
+                    .to_frame()
+                )
+                sources['id'] = sources.src_dj.apply(getattr, args=('id',))
+                src_dj_obj = src_dj_obj.append(sources)
+                continue
 
             # 1.3 get the image sources and save them in DB
             sources = image.read_selavy(img)
             logger.info(f'Processed sources dataframe of shape: {sources.shape}')
 
             # do DB bulk create
-            sources['dj_model'] = sources.apply(get_source_models, axis=1)
+            sources['src_dj'] = sources.apply(get_source_models, axis=1)
             # do a upload without evaluate the objects, that should be faster
             # see https://docs.djangoproject.com/en/2.2/ref/models/querysets/
-            # TODO: remove development operations
-            if Source.objects.filter(image_id=img.id).exists():
-                del_out = Source.objects.filter(image_id=img.id).delete()
-                logger.info(f'deleting all sources for this image: {del_out}')
+            # # TODO: remove development operations
+            # if Source.objects.filter(image_id=img.id).exists():
+            #     del_out = Source.objects.filter(image_id=img.id).delete()
+            #     logger.info(f'deleting all sources for this image: {del_out}')
 
-            batch_size = 1_000
-            for idx in range(0, sources.dj_model.size, batch_size):
+            batch_size = 10_000
+            for idx in range(0, sources.src_dj.size, batch_size):
                 out_bulk = Source.objects.bulk_create(
-                    sources.dj_model.iloc[idx : idx + batch_size].values.tolist(),
+                    sources.src_dj.iloc[idx : idx + batch_size].values.tolist(),
                     batch_size
                 )
                 logger.info(f'bulk uploaded #{len(out_bulk)} sources')
 
             # make a columns with the source id
-            sources['id'] = sources.dj_model.apply(getattr, args=('id',))
+            sources['id'] = sources.src_dj.apply(getattr, args=('id',))
+            src_dj_obj = src_dj_obj.append(sources.loc[:, ['id','src_dj']])
 
             # save sources to parquet file in dataset folder
             if not os.path.exists(os.path.dirname(img.sources_path)):
                 os.mkdir(os.path.dirname(img.sources_path))
-            sources.drop('dj_model', axis=1).to_parquet(
+            sources.drop('src_dj', axis=1).to_parquet(
                 img.sources_path,
                 index=False
             )
             del sources, image, band_id, img, out_bulk
 
         # STEP #2: source association
-        # 2.1 Associate Sources with reference catalogues
+        # 2.1 Associate Sources with reference catalogs
         if SurveySource.objects.exists():
             pass
 
@@ -117,7 +136,7 @@ class Pipeline():
             ra=skyc1_srcs.ra * u.degree,
             dec=skyc1_srcs.dec * u.degree
         )
-        # initialise the df of catalogues with the base one
+        # initialise the df of catalogs with the base one
         catalogs_df = pd.DataFrame()
         for it, image in enumerate(images[1:]):
             logger.info(f'Association iteration: #{it + 1}')
@@ -141,12 +160,12 @@ class Pipeline():
             skyc1_srcs.loc[ sel & nan_sel, 'cat'] = (
                 skyc1_srcs.index[ sel & nan_sel].values + start_elem + 1.
             )
-            # append skyc1 selection to catalogue df
+            # append skyc1 selection to catalog df
             catalogs_df = catalogs_df.append(skyc1_srcs)
 
             # assign catalog temp id to skyc2 sorces from skyc1
             skyc2_srcs.loc[idx[sel], 'cat'] = skyc1_srcs.loc[sel, 'cat'].values
-            # append skyc2 selection to catalogue df
+            # append skyc2 selection to catalog df
             catalogs_df = catalogs_df.append(skyc2_srcs.loc[idx[sel]])
 
             # update skyc1 and df for next association iteration
@@ -179,7 +198,7 @@ class Pipeline():
             catalogs_df.index[ nan_sel].values + start_elem
         )
 
-        # tidy the df of catalogues to drop duplicated entries
+        # tidy the df of catalogs to drop duplicated entries
         # to have unique rows of c_name and src_id
         catalogs_df = catalogs_df.drop_duplicates()
 
@@ -190,12 +209,45 @@ class Pipeline():
             .rename(columns={'ra':'ave_ra', 'dec':'ave_dec'})
         )
         # generate the catalog models
-        cat_df['dj_model'] = cat_df.apply(get_catalog_models, axis=1)
-        catalogs_df = catalogs_df.merge(cat_df, on='cat')
+        cat_df['cat_dj'] = cat_df.apply(
+            get_catalog_models,
+            axis=1,
+            dataset=dataset
+        )
+        # create catalogs in DB
+        # TODO remove deleting existing catalogs
+        if Catalog.objects.filter(dataset=dataset).exists():
+            cats = Catalog.objects.filter(dataset=dataset).delete()
+            logger.info(f'deleting all catalogs for this dataset: {cats}')
+
+        batch_size = 10_000
+        for idx in range(0, cat_df.cat_dj.size, batch_size):
+            out_bulk = Catalog.objects.bulk_create(
+                cat_df.cat_dj.iloc[idx : idx + batch_size].tolist(),
+                batch_size
+            )
+            logger.info(f'bulk created #{len(out_bulk)} catalogs')
+
+        catalogs_df = (
+            catalogs_df.merge(cat_df, on='cat')
+            .merge(src_dj_obj, on='id')
+        )
         del cat_df
 
-        # insert association in DB
-        import ipdb; ipdb.set_trace()  # breakpoint c6ac6b08 //
+        # Create Associan objects (linking sources and catalogs) and insert in DB
+        catalogs_df['assoc_dj'] = catalogs_df.apply(
+            lambda row: Association(
+                source=row['src_dj'],
+                catalog=row['cat_dj']
+            ), axis=1
+        )
+        batch_size = 10_000
+        for idx in range(0, catalogs_df.assoc_dj.size, batch_size):
+            out_bulk = Association.objects.bulk_create(
+                catalogs_df.assoc_dj.iloc[idx : idx + batch_size].tolist(),
+                batch_size
+            )
+            logger.info(f'bulk created #{len(out_bulk)} associations')
 
         # STEP #3: ...
         pass
@@ -230,7 +282,7 @@ class Pipeline():
     def get_create_img(dataset, band_id, image):
         img = Image.objects.filter(name__exact=image.name)
         if img:
-            return img.get()
+            return (img.get(), True)
 
         # at this stage source parquet file not created but assume location
         sources_path = os.path.join(
@@ -252,4 +304,4 @@ class Pipeline():
                 setattr(img, fld.attname, getattr(image, fld.attname))
         img.save()
 
-        return img
+        return (img, False)
