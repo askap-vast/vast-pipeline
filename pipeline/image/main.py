550228c645d6a1cc2d70d3731f656163c8714821
Serg <sergio.pintaldi@gmail.com>
2020-01-07 11:14:12 +1100

    fix get cdelt

--- pipeline/image/main.py
+++ pipeline/image/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from astropy.io import fits
 from astropy.wcs import WCS
+from astropy.wcs.utils import proj_plane_pixel_scales
 
 from pipeline.survey.translators import tr_selavy
 
@@ -73,8 +74,6 @@ class FitsImage(Image):
                 'header': header,
                 'fits_naxis1': 'NAXIS1',
                 'fits_naxis2': 'NAXIS2',
-                'fits_cdelt1': 'CDELT1',
-                'fits_cdelt2': 'CDELT2'
             }
 
             # set the coordinate attributes
@@ -84,7 +83,7 @@ class FitsImage(Image):
         self.jd = self.time.to_julian_date()
 
 
-    def __get_img_coordinates(self, header, fits_naxis1, fits_naxis2, fits_cdelt1, fits_cdelt2):
+    def __get_img_coordinates(self, header, fits_naxis1, fits_naxis2):
         """
         set the image attributes ra, dec, fov_bmin and fov_bmaj, radius
         from the image file header
@@ -104,8 +103,9 @@ class FitsImage(Image):
         # TODO: move unused pixel as argument
         unusedpix = 0.
         usable_radius_pix = self.__get_radius_pixels(header, fits_naxis1, fits_naxis2) - unusedpix
-        self.fov_bmin = usable_radius_pix * abs(header[fits_cdelt1])
-        self.fov_bmaj = usable_radius_pix * abs(header[fits_cdelt2])
+        cdelt1, cdelt2 = self.__get_fits_cdelt(header)
+        self.fov_bmin = usable_radius_pix * abs(cdelt1)
+        self.fov_bmaj = usable_radius_pix * abs(cdelt2)
 
         # set the pixels radius
         # TODO: check calcs
@@ -123,6 +123,13 @@ class FitsImage(Image):
             # We simply place the largest circle we can in the centre.
             diameter = min(header[fits_naxis1], header[fits_naxis2])
         return diameter / 2.
+
+    @staticmethod
+    def __get_fits_cdelt(header):
+        if ('CD1_1' in header) and ('CD1_2' in header):
+            return proj_plane_pixel_scales(WCS(header).celestial)
+        else:
+            return header['CDELT1'], header['CDELT2']
 
     def __get_frequency(self, header):
         """
