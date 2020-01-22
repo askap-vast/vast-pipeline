abeb2f4ccc2d7f6c12998f456408c447b877b9a1
Srg <sergio.pintaldi@gmail.com>
2020-01-22 18:18:33 +1100

    start aladin integration

---
+++ static/js/aladin-script.js
@@ -0,0 +1,8 @@
+let aladinConf = JSON.parse(document.getElementById('data-aladin').textContent);
+var aladin = A.aladin('#aladinViz');
+aladin.setZoom(0.36);
+aladin.gotoPosition( aladinConf.ave_ra , aladinConf.ave_dec);
+var sumss = aladin.createImageSurvey('SUMSS', 'SUMSS', 'http://alasky.u-strasbg.fr/SUMSS', 'equatorial', 6, {imgFormat: 'png'});
+var nvss = aladin.createImageSurvey('NVSS', 'NVSS', 'http://alasky.u-strasbg.fr/NVSS/intensity/', 'equatorial', 5, {imgFormat: 'jpg'});
+aladin.setImageSurvey(sumss);
+aladin.getBaseImageLayer().getColorMap().reverse();
