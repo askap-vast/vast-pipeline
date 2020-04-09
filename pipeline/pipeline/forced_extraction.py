f5e115168ce2991355ca375b0eb30c6d19e1eb18
Sergio <sergio.pintaldi@gmail.com>
2020-04-09 15:22:10 +1000

    saving temporary changes

--- pipeline/pipeline/forced_extraction.py
+++ pipeline/pipeline/forced_extraction.py
@@ -1,3 +1,5 @@
+# import faulthandler; faulthandler.enable()
+
 import logging
 import numpy as np
 import pandas as pd
@@ -25,7 +27,7 @@ def forced_extraction(srcs_df, sources_df):
     ))
     skyreg_df = (
         skyreg_df.groupby(skyreg_df.columns.drop('name').values.tolist())
-        .agg(lambda group: group.values.ravel().tolist())
+        .agg(lambda group: [group.values.ravel()])
         .reset_index()
         .rename(columns={'name':'img_list'})
     )
@@ -38,7 +40,7 @@ def forced_extraction(srcs_df, sources_df):
     cols = [
         'wavg_uncertainty_ew', 'wavg_uncertainty_ns', 'avg_flux_int',
         'avg_flux_peak', 'max_flux_peak', 'v_int', 'v_peak', 'eta_int',
-        'eta_peak', 'new', 'related_list'
+        'eta_peak', 'new', 'related_list', 'src_dj'
     ]
     src_skyrg_df = cross_join(
         (
@@ -55,5 +57,19 @@ def forced_extraction(srcs_df, sources_df):
             np.deg2rad(src_skyrg_df['centre_dec'].values),
         )
     )
+
+    # select rows where separation is less than sky region radius
+    # drop not more useful columns and groupby source id
+    # compute list of images
+    src_skyrg_df = (
+        src_skyrg_df.loc[
+            src_skyrg_df['sep'] < src_skyrg_df['xtr_radius'],
+            ['source', 'img_list']
+        ]
+        # .set_index('source')
+        # .groupby('source')
+        # .agg(lambda group: group.values.ravel().unique().tolist())
+    )
+    import pdb; pdb.set_trace()  # breakpoint 25334aa5 //
 
     pass
