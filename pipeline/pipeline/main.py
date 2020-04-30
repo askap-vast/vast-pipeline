d99a188570fedb7e6adf9147ef7117dfd4f0eeaf
Adam Stewart <adam.stewart73@gmail.com>
2020-04-30 10:52:54 +1000

    Removed references and settings to max_img

--- pipeline/pipeline/main.py
+++ pipeline/pipeline/main.py
@@ -21,15 +21,12 @@ class Pipeline():
     just one is used)
     '''
 
-    def __init__(self, max_img=10, config=None):
+    def __init__(self, config=None):
         '''
         We limit the size of the cube cache so we don't hit the max
         files open limit or use too much RAM
         '''
-        self.max_img = max_img
         self.config = config
-        if self.config.MAX_BACKWARDS_MONITOR_IMAGES:
-            self.max_img=self.config.MAX_BACKWARDS_MONITOR_IMAGES + 1
 
         # A dictionary of path to Fits images, eg
         # "/data/images/I1233234.FITS" and selavy catalogues
