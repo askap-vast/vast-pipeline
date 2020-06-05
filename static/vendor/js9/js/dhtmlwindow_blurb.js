8755c02fae2c08f271c2f2b28607e47b2675308b
Sergio <sergio.pintaldi@gmail.com>
2020-06-05 11:55:14 +1000

    removed js9 vendor files

--- static/vendor/js9/js/dhtmlwindow_blurb.js
+++
@@ -1,5 +0,0 @@
-  /***********************************************
-  * DHTML Window Widget- © Dynamic Drive (www.dynamicdrive.com)
-  * This notice must stay intact for legal use.
-  * Visit http://www.dynamicdrive.com/ for full source code
-  ***********************************************/
