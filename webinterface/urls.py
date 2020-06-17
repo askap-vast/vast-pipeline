9be4bb4adf2ae64b414db9623cac3c30273d11ae
Sergio <sergio.pintaldi@gmail.com>
2020-06-17 14:14:45 +1000

    added login logout, change prefix social auth urls

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -1,27 +1,15 @@
-"""pipelinedev URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic import TemplateView
-from pipeline.views import Home
+from pipeline.views import Home, Login
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home, name='index'),
+    path('login/', Login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('pipeline.urls')),
-    path('', include('social_django.urls', namespace='social_login')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
