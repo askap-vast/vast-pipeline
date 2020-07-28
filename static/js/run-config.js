ac46eeaecc5fb0c7e3d3c17efa92cf6e8b8c85f1
Sergio <sergio.pintaldi@gmail.com>
2020-07-28 12:04:08 +1000

    fix indentation add disable menu based on monitor

--- static/js/run-config.js
+++ static/js/run-config.js
@@ -1,30 +1,39 @@
 $(document).ready(function() {
 
-    $("#RunConfigButton").on('click', function(e) {
-        // e.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: "/api/rawimages/",
-            success: function(result) {
-                // populate the images and selavy menus
-                // console.log(result);
-                 $.each(result.fits, function (idx, item) {
-                    $("#imagesFilesDropDown").append('<option>' + item + '</option>');
-                    $("#bkgFilesDropDown").append('<option>' + item + '</option>');
-                    $("#noiseFilesDropDown").append('<option>' + item + '</option>');
-                 });
-                 $.each(result.selavy, function (idx, item) {
-                    $("#selavyFilesDropDown").append('<option>' + item + '</option>');
-                 });
-                 $('#imagesFilesDropDown').selectpicker('refresh');
-                 $('#selavyFilesDropDown').selectpicker('refresh');
-                 $('#bkgFilesDropDown').selectpicker('refresh');
-                 $('#noiseFilesDropDown').selectpicker('refresh');
-            },
-            error: function(result) {
-                alert('error');
-            }
-        });
+  $("#RunConfigButton").on('click', function(e) {
+    $.ajax({
+      type: "GET",
+      url: "/api/rawimages/",
+      success: function(result) {
+        // populate the images and selavy menus
+        // console.log(result);
+         $.each(result.fits, function (idx, item) {
+          $("#imagesFilesDropDown").append('<option>' + item + '</option>');
+          $("#bkgFilesDropDown").append('<option>' + item + '</option>');
+          $("#noiseFilesDropDown").append('<option>' + item + '</option>');
+         });
+         $.each(result.selavy, function (idx, item) {
+          $("#selavyFilesDropDown").append('<option>' + item + '</option>');
+         });
+         $('#imagesFilesDropDown').selectpicker('refresh');
+         $('#selavyFilesDropDown').selectpicker('refresh');
+         $('#bkgFilesDropDown').selectpicker('refresh');
+         $('#noiseFilesDropDown').selectpicker('refresh');
+      },
+      error: function(result) {
+        alert('error');
+      }
     });
+  });
+
+  $("#monitorSwitch").on('click', function(e) {
+    if (this.checked) {
+      $("#bkgFilesDropDown").attr('disabled',false).selectpicker('refresh');
+      $("#noiseFilesDropDown").attr('disabled',false).selectpicker('refresh');
+    } else {
+      $("#bkgFilesDropDown").attr('disabled',true).selectpicker('refresh');
+      $("#noiseFilesDropDown").attr('disabled',true).selectpicker('refresh');
+    };
+  });
 
 });
