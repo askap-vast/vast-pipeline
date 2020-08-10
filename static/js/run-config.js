910a0d653d1b8406b4eba14b31014286608a9a99
Adam Stewart <adam.stewart73@gmail.com>
2020-08-10 16:31:54 +1000

    Moved tooltip js

--- static/js/run-config.js
+++ static/js/run-config.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  $('[data-toggle="tooltip"]').tooltip();
+
   $("#newPipeRunButton").on('click', function(e) {
     // hide second part of form
     $("#pipeRunConfigForm").hide();
