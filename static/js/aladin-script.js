2a20b60c2829df4fab7d1cf80ee7d206d33f982e
Andrew O'Brien <obrienan@uwm.edu>
2020-09-15 17:44:24 -0500

    load aladin after external search table renders

--- static/js/aladin-script.js
+++ static/js/aladin-script.js
@@ -1,19 +1,23 @@
-let aladinConf = JSON.parse(document.getElementById('data-aladin').textContent);
-var aladin = A.aladin('#aladinViz');
-aladin.setZoom(aladinConf.aladin_zoom);
-aladin.gotoPosition(aladinConf.aladin_ra , aladinConf.aladin_dec);
-var sumss = aladin.createImageSurvey('SUMSS', 'SUMSS', 'https://alasky.u-strasbg.fr/SUMSS', 'equatorial', 6, {imgFormat: 'png'});
-var nvss = aladin.createImageSurvey('NVSS', 'NVSS', 'https://alasky.u-strasbg.fr/NVSS/intensity/', 'equatorial', 5, {imgFormat: 'jpg'});
-let survey = (aladinConf.aladin_dec < -40) ? sumss : nvss;
-aladin.setImageSurvey(survey);
-aladin.getBaseImageLayer().getColorMap().reverse();
-if (aladinConf.hasOwnProperty('aladin_box_ra')) {
-    var overlay = A.graphicOverlay({color: '#ee2345', lineWidth: 2});
-    aladin.addOverlay(overlay);
-    overlay.addFootprints([A.polygon([
-        [aladinConf.aladin_ra + (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec - (aladinConf.aladin_box_dec / 2)],
-        [aladinConf.aladin_ra + (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec + (aladinConf.aladin_box_dec / 2)],
-        [aladinConf.aladin_ra - (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec + (aladinConf.aladin_box_dec / 2)],
-        [aladinConf.aladin_ra - (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec - (aladinConf.aladin_box_dec / 2)],
-    ])]);
-}
+// wait until the external search results table has fully loaded to ensure Aladin Lite
+// occupies the full height of the  parent element
+$('#externalResultsTable').on("init.dt", function() {
+    let aladinConf = JSON.parse(document.getElementById('data-aladin').textContent);
+    var aladin = A.aladin('#aladinViz', {fov: 1});
+    aladin.setZoom(aladinConf.aladin_zoom);
+    aladin.gotoPosition(aladinConf.aladin_ra , aladinConf.aladin_dec);
+    var sumss = aladin.createImageSurvey('SUMSS', 'SUMSS', 'https://alasky.u-strasbg.fr/SUMSS', 'equatorial', 6, {imgFormat: 'png'});
+    var nvss = aladin.createImageSurvey('NVSS', 'NVSS', 'https://alasky.u-strasbg.fr/NVSS/intensity/', 'equatorial', 5, {imgFormat: 'jpg'});
+    let survey = (aladinConf.aladin_dec < -40) ? sumss : nvss;
+    aladin.setImageSurvey(survey);
+    aladin.getBaseImageLayer().getColorMap().reverse();
+    if (aladinConf.hasOwnProperty('aladin_box_ra')) {
+        var overlay = A.graphicOverlay({color: '#ee2345', lineWidth: 2});
+        aladin.addOverlay(overlay);
+        overlay.addFootprints([A.polygon([
+            [aladinConf.aladin_ra + (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec - (aladinConf.aladin_box_dec / 2)],
+            [aladinConf.aladin_ra + (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec + (aladinConf.aladin_box_dec / 2)],
+            [aladinConf.aladin_ra - (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec + (aladinConf.aladin_box_dec / 2)],
+            [aladinConf.aladin_ra - (aladinConf.aladin_box_ra / 2) , aladinConf.aladin_dec - (aladinConf.aladin_box_dec / 2)],
+        ])]);
+    }
+})
