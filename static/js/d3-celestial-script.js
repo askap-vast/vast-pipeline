3e93bea82519c1b7b327ba889601b16e2a479fc6
Adam Stewart <adam.stewart73@gmail.com>
2020-11-16 17:51:43 +1100

    Disable d3 celestial Zenith follow animation

--- static/js/d3-celestial-script.js
+++ static/js/d3-celestial-script.js
@@ -10,7 +10,7 @@ var config = {
   orientationfixed: true,  // Keep orientation angle the same as center[2]
   geopos: null,       // optional initial geographic position [lat,lon] in degrees,
                       // overrides center
-  follow: "zenith",   // on which coordinates to center the map, default: zenith, if location enabled,
+  follow: "center",   // on which coordinates to center the map, default: zenith, if location enabled,
                       // otherwise center
   zoomlevel: null,    // initial zoom level 0...zoomextend; 0|null = default, 1 = 100%, 0 < x <= zoomextend
   zoomextend: 10,     // maximum zoom level
