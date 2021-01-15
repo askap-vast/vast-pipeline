57253e6fd56470f8986909c25ebadd6ecf78e614
Adam Stewart <adam.stewart73@gmail.com>
2021-01-15 23:39:38 +1100

    Working restore option after add

--- vast_pipeline/management/commands/restorepiperun.py
+++ vast_pipeline/management/commands/restorepiperun.py
@@ -1,11 +1,18 @@
 import os
 import logging
 import shutil
+import numpy as np
+import pandas as pd
 
 from glob import glob
+from django.db.models import Q
+from django.db import transaction
 from django.core.management.base import BaseCommand, CommandError
 
-from vast_pipeline.models import Run
+from vast_pipeline.models import (
+    Run, Source, Measurement, Image, Association, MeasurementPair
+)
+from vast_pipeline.pipeline.loading import update_sources
 from vast_pipeline.pipeline.main import Pipeline
 from ..helpers import get_p_run_name
 
@@ -28,19 +35,21 @@ def yesno(question):
 def restore_pipe(p_run, bak_files, prev_config):
     # check images match
     img_f_list = getattr(prev_config, 'IMAGE_FILES')
-    img_f_list = [
-        item for sublist in img_f_list.values() for item in sublist
-    ]
+    if isinstance(img_f_list, dict):
+        img_f_list = [
+            item for sublist in img_f_list.values() for item in sublist
+        ]
+
     prev_images = pd.read_parquet(
         bak_files['images'], columns=['id', 'measurements_path']
     )
 
-    if prev_images.shape[0] != len(img_f_list):
-        raise CommandError(
-            'Number of images in previous config file does not'
-            ' match the number found in previous images.parquet.bak.'
-            ' Cannot restore pipeline run.'
-        )
+    # if prev_images.shape[0] != len(img_f_list):
+    #     raise CommandError(
+    #         'Number of images in previous config file does not'
+    #         ' match the number found in previous images.parquet.bak.'
+    #         ' Cannot restore pipeline run.'
+    #     )
 
     # check forced measurements
     monitor = getattr(prev_config, 'MONITOR')
@@ -60,7 +69,7 @@ def restore_pipe(p_run, bak_files, prev_config):
             bak_meas_id = pd.read_parquet(
                 bak_files['associations'],
                 columns = ['meas_id']
-            ).unique()
+            )['meas_id'].unique()
 
             # load backup forced measurements
             forced_meas = pd.concat(
@@ -75,11 +84,14 @@ def restore_pipe(p_run, bak_files, prev_config):
             )
 
             # Get forced ids from the associations
-            forced_meas_id = bak_meas_id.isin(
-                meas['id'].to_numpy(), invert=True
-            )
+            forced_meas_id = bak_meas_id[
+                np.isin(bak_meas_id, meas['id'].to_numpy(), invert=True)
+            ]
 
-            if np.sort(forced_meas_id) != np.sort(forced_meas['id'].to_numpy()):
+            if not np.array_equal(
+                np.sort(forced_meas_id),
+                np.sort(forced_meas['id'].to_numpy())
+            ):
                 raise CommandError(
                     'The forced measurements .bak files do not match the'
                     ' previous run.'
@@ -88,16 +100,15 @@ def restore_pipe(p_run, bak_files, prev_config):
 
             del meas
 
-    logger.info("Restoring '%s' from backup parquet files.", p_run_name)
+    logger.info("Restoring '%s' from backup parquet files.", p_run.name)
 
     # Delete any new sources
-    bak_source_ids = pd.read_parquet(bak_files['sources'], columns=['id'])
-
+    bak_sources = pd.read_parquet(bak_files['sources'])
 
     sources_to_delete = (
         Source.objects
         .filter(run=p_run)
-        .exclude(id__in=bak_source_ids['id'].to_numpy())
+        .exclude(id__in=bak_sources.index.to_numpy())
     )
 
     if sources_to_delete.exists():
@@ -122,12 +133,14 @@ def restore_pipe(p_run, bak_files, prev_config):
         )
 
         ids_to_delete = current_forced_meas.loc[
-            ~current_forced_meas.isin(forced_meas['id'].to_numpy()),
+            ~current_forced_meas['id'].isin(forced_meas['id'].to_numpy()),
             'id'
         ]
 
         meas_to_delete = Measurement.objects.filter(id__in=ids_to_delete)
+
         del ids_to_delete
+
         if meas_to_delete.exists():
             with transaction.atomic():
                 n_del, detail_del = meas_to_delete.delete()
@@ -138,6 +151,84 @@ def restore_pipe(p_run, bak_files, prev_config):
                 )
                 logger.debug('(type, #deleted): %s', detail_del)
 
+    # restore source metrics
+    bak_sources = update_sources(bak_sources)
+
+    # remove images from run
+    images_to_remove = (
+        Image.objects
+        .filter(run=p_run)
+        .exclude(id__in=prev_images['id'].to_numpy())
+    )
+
+    if images_to_remove.exists():
+        with transaction.atomic():
+            p_run.image_set.remove(*images_to_remove)
+
+    # load image meas
+    meas = pd.concat(
+        [pd.read_parquet(
+            i, columns=['id']
+        ) for i in prev_images['measurements_path']]
+    )
+
+    association_criteria_1 = Q(source_id__in=bak_sources['id'].to_numpy())
+    association_criteria_2 = ~Q(meas_id__in=meas['id'].to_numpy())
+    associations_to_delete = Association.objects.filter(
+        association_criteria_1 and association_criteria_2
+    )
+
+    if associations_to_delete.exists():
+        with transaction.atomic():
+            n_del, detail_del = associations_to_delete.delete()
+            logger.info(
+                ('Deleting associations to restore run.'
+                 ' Total objects deleted: %i'),
+                n_del,
+            )
+            logger.debug('(type, #deleted): %s', detail_del)
+
+    pair_criteria_1 = Q(source_id__in=bak_sources['id'].to_numpy())
+    pair_criteria_2 = ~Q(measurement_a__in=meas['id'].to_numpy())
+    pair_criteria_3 = ~Q(measurement_b__in=meas['id'].to_numpy())
+
+    pairs_to_delete = MeasurementPair.objects.filter(
+       pair_criteria_1 and (pair_criteria_2 | pair_criteria_3)
+    )
+
+    if pairs_to_delete.exists():
+        with transaction.atomic():
+            n_del, detail_del = pairs_to_delete.delete()
+            logger.info(
+                ('Deleting measurement pairs to restore run.'
+                 ' Total objects deleted: %i'),
+                n_del,
+            )
+            logger.debug('(type, #deleted): %s', detail_del)
+
+    p_run.n_images = prev_images.shape[0]
+    p_run.n_sources = bak_sources.shape[0]
+    p_run.n_selavy_measurements = meas.shape[0]
+    if monitor:
+        p_run.n_forced_measurements = forced_meas.shape[0]
+
+    with transaction.atomic():
+        p_run.save()
+
+    # switch files and delete backups
+    for i in bak_files:
+        bak_file = bak_files[i]
+        actual_file = bak_file.replace('.bak', '')
+        shutil.copy(bak_file, actual_file)
+        os.remove(bak_file)
+
+    if monitor:
+        for i in current_forced_parquets:
+            os.remove(i)
+
+        for i in forced_parquets:
+            new_file = i.replace('.bak', '')
+            shutil.copy(i, new_file)
 
 
 class Command(BaseCommand):
@@ -191,10 +282,16 @@ class Command(BaseCommand):
             except Run.DoesNotExist:
                 raise CommandError(f'Pipeline run {p_run_name} does not exist')
 
+            path = p_run.path
+            pipeline = Pipeline(
+                name=p_run_name,
+                config_path=os.path.join(path, 'config.py')
+            )
+
             prev_config = os.path.join(p_run.path, 'config_prev.py')
 
             if os.path.isfile(prev_config):
-                prev_config = p_run.load_cfg(prev_config)
+                prev_config = Pipeline.load_cfg(prev_config)
             else:
                 raise CommandError(
                     f'Previous config file does not exist.'
@@ -203,10 +300,10 @@ class Command(BaseCommand):
 
             bak_files = {}
             for i in [
-                'associations', 'bands.parquet', 'images', 'measurement_pairs',
+                'associations', 'bands', 'images', 'measurement_pairs',
                 'relations', 'skyregions', 'sources'
             ]:
-                parquet = os.path.join(p_run.path, f'{i}.parquet')
+                parquet = os.path.join(p_run.path, f'{i}.parquet.bak')
 
                 if os.path.isfile(parquet):
                     bak_files[i] = parquet
@@ -216,8 +313,9 @@ class Command(BaseCommand):
                         ' Cannot restore pipeline run.'
                     )
 
-
             user_continue = yesno("Would you like to restore the run?")
 
             if user_continue:
                 restore_pipe(p_run, bak_files, prev_config)
+
+            logger.info('Restore complete.')
