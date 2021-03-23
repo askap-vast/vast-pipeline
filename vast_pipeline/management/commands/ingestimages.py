6453a8c921038b3065a556ebb23424d6de2e86d8
David Liptai <dliptai@swin.edu.au>
2021-03-23 17:22:55 +1100

    Fixed ingestimages command to ingest images separately from a pipeline run

--- vast_pipeline/management/commands/ingestimages.py
+++ vast_pipeline/management/commands/ingestimages.py
@@ -10,6 +10,22 @@ from ..helpers import get_p_run_name
 
 logger = logging.getLogger(__name__)
 
+class _DummyConfig(object):
+
+    def __init__(self,image_files,selavy_files,noise_files,background_files):
+        self.IMAGE_FILES = image_files
+        self.SELAVY_FILES = selavy_files
+        self.NOISE_FILES = noise_files
+        self.BACKGROUND_FILES = background_files
+
+class _DummyPipeline(object):
+    make_image_paths = Pipeline.match_images_to_data
+
+    def __init__(self,image_files,selavy_files,noise_files,background_files):
+        self.config = _DummyConfig(image_files,selavy_files,noise_files,background_files)
+        self.config, _ = Pipeline.check_for_epoch_based(self.config)
+        self.make_image_paths()
+
 
 class Command(BaseCommand):
     """
@@ -21,18 +37,38 @@ class Command(BaseCommand):
     )
 
     def add_arguments(self, parser):
-        # positional arguments
         parser.add_argument(
-            'piperun',
+            '-i',
+            '--image-files',
+            nargs='+',
             type=str,
-            help='Path or name of the pipeline run.'
+            required=True,
+            help=('Paths to image files.')
         )
-
-        # parser.add_argument(
-        #     'imgpath',
-        #     type=str,
-        #     help='Path of the image.'
-        # )
+        parser.add_argument(
+            '-s',
+            '--selavy-files',
+            nargs='+',
+            type=str,
+            required=True,
+            help=('Paths to selavy files.')
+        )
+        parser.add_argument(
+            '-n',
+            '--noise-files',
+            nargs='+',
+            type=str,
+            required=True,
+            help=('Paths to noise files.')
+        )
+        parser.add_argument(
+            '-b',
+            '--background-files',
+            nargs='+',
+            type=str,
+            required=True,
+            help=('Paths to background files.')
+        )
 
     def handle(self, *args, **options):
         # configure logging
@@ -43,32 +79,25 @@ class Command(BaseCommand):
             # set the traceback on
             options['traceback'] = True
 
-        piperun = options['piperun']
+        n_images = len(options['image_files'])
+        n_selavy = len(options['selavy_files'])
+        n_noise = len(options['noise_files'])
+        n_background = len(options['background_files'])
 
-        p_run_name, run_folder = get_p_run_name(
-            piperun,
-            return_folder=True
+        if not n_images == n_selavy:
+            raise CommandError("Number of selavy files not equal to number of image files")
+
+        if not n_images == n_noise:
+            raise CommandError("Number of noise files not equal to number of image files")
+
+        if not n_images == n_background:
+            raise CommandError("Number of background files not equal to number of image files")
+
+        d = _DummyPipeline(
+            options['image_files'],
+            options['selavy_files'],
+            options['noise_files'],
+            options['background_files'],
         )
-        try:
-            p_run = Run.objects.get(name=p_run_name)
-        except Run.DoesNotExist:
-            raise CommandError(f'Pipeline run {p_run_name} does not exist')
 
-        if p_run.status != 'END':
-            raise CommandError(f'Pipeline run {p_run_name} has not completed.')
-
-        path = p_run.path
-        pipeline = Pipeline(
-            name=p_run.name,
-            config_path=os.path.join(path, 'config.py')
-        )
-
-        pipeline.validate_cfg()
-
-        pipeline.match_images_to_data()
-
-        images, skyregs_df = make_upload_images(
-            pipeline.img_paths,
-            pipeline.config,
-            p_run
-        )
+        make_upload_images(d.img_paths)
