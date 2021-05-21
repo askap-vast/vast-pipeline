256e1408ff7f395bb50236a907bf9baf70d17ce2
Andrew O'Brien <obrienan@uwm.edu>
2021-05-21 10:44:10 -0500

    define reverse data migration

--- vast_pipeline/migrations/0004_source_name_change.py
+++ vast_pipeline/migrations/0004_source_name_change.py
@@ -16,13 +16,29 @@ def update_source_names(apps, schema_editor):
                 source.save()
 
 
+def reverse_update_source_names(apps, schema_editor):
+    """Update source names from J... to the v0.2.0 ASKAP_... convention"""
+    Source = apps.get_model("vast_pipeline", "Source")
+    while Source.objects.filter(name__startswith="J").exists():
+        # do the updates in transaction batches of 1000 in case the source table is large
+        with transaction.atomic():
+            for source in Source.objects.filter(name__startswith="J")[:1000]:
+                source.name = (
+                    f"ASKAP_{deg2hms(source.wavg_ra, precision=2)}"
+                    f"{deg2dms(source.wavg_dec, precision=2)}"
+                ).replace(":", "")
+                source.save()
+
+
 class Migration(migrations.Migration):
     atomic = False  # disable transactions, the source table may be large
 
     dependencies = [
-        ('vast_pipeline', '0003_auto_20210402_2136'),
+        ("vast_pipeline", "0003_auto_20210402_2136"),
     ]
 
     operations = [
-        migrations.RunPython(update_source_names),
+        migrations.RunPython(
+            update_source_names, reverse_code=reverse_update_source_names
+        ),
     ]
