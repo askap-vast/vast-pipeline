d6194a0a1271ba5582b34ef4f0ff77fc30afa72c
David Liptai <dliptai@swin.edu.au>
2021-06-03 16:36:50 +1000

    remove unused import

--- vast_pipeline/management/commands/initingest.py
+++ vast_pipeline/management/commands/initingest.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from vast_pipeline.pipeline.config import ImageIngestConfig, make_config_template
