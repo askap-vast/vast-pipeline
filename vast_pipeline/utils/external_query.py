fffb3406b2258ccc23cf12a49b81801ce508bdcb
Andrew O'Brien <obrienan@uwm.edu>
2021-06-11 15:56:25 -0500

    added TNS external query Closes #498

--- vast_pipeline/utils/external_query.py
+++ vast_pipeline/utils/external_query.py
@@ -1,26 +1,31 @@
+import json
 from typing import Any, Dict, List
+from urllib.parse import urljoin
 
 from astropy.coordinates import SkyCoord, Angle, Longitude, Latitude
 from astroquery.simbad import Simbad
 from astroquery.ned import Ned
+from django.conf import settings
+import requests
 
 
 def simbad(coord: SkyCoord, radius: Angle) -> List[Dict[str, Any]]:
     """Perform a cone search for sources with SIMBAD.
 
     Args:
-        coord: the coordinate of the centre of the cone.
-        radius: the radius of the cone in angular units.
+        coord: The coordinate of the centre of the cone.
+        radius: The radius of the cone in angular units.
 
     Returns:
         A list of dicts, where each dict is a query result row with the following keys:
-            object_name: the name of the astronomical object.
-            database: the source of the result, i.e. SIMBAD.
-            separation_arcsec: separation to the query coordinate in arcsec.
-            otype: object type.
-            otype_long: long form of the object type.
-            ra_hms: RA coordinate string in <HH>h<MM>m<SS.SSS>s format.
-            dec_dms: Dec coordinate string in ±<DD>d<MM>m<SS.SSS>s format.
+
+        - object_name: the name of the astronomical object.
+        - database: the source of the result, i.e. SIMBAD.
+        - separation_arcsec: separation to the query coordinate in arcsec.
+        - otype: object type.
+        - otype_long: long form of the object type.
+        - ra_hms: RA coordinate string in hms format.
+        - dec_dms: Dec coordinate string in ±dms format.
     """
     CustomSimbad = Simbad()
     CustomSimbad.add_votable_fields(
@@ -62,18 +67,19 @@ def ned(coord: SkyCoord, radius: Angle) -> List[Dict[str, Any]]:
     """Perform a cone search for sources with NED.
 
     Args:
-        coord: the coordinate of the centre of the cone.
-        radius: the radius of the cone in angular units.
+        coord: The coordinate of the centre of the cone.
+        radius: The radius of the cone in angular units.
 
     Returns:
         A list of dicts, where each dict is a query result row with the following keys:
-            object_name: the name of the astronomical object.
-            database: the source of the result, i.e. NED.
-            separation_arcsec: separation to the query coordinate in arcsec.
-            otype: object type.
-            otype_long: long form of the object type.
-            ra_hms: RA coordinate string in <HH>h<MM>m<SS.SSS>s format.
-            dec_dms: Dec coordinate string in ±<DD>d<MM>m<SS.SSS>s format.
+
+        - object_name: the name of the astronomical object.
+        - database: the source of the result, i.e. NED.
+        - separation_arcsec: separation to the query coordinate in arcsec.
+        - otype: object type.
+        - otype_long: long form of the object type.
+        - ra_hms: RA coordinate string in hms format.
+        - dec_dms: Dec coordinate string in ±dms format.
     """
     # NED API doesn't supply the long-form object types.
     # Copied from https://ned.ipac.caltech.edu/Documents/Guides/Database
@@ -174,3 +180,69 @@ def ned(coord: SkyCoord, radius: Angle) -> List[Dict[str, Any]]:
             orient="records"
         )
     return ned_results_dict_list
+
+
+def tns(coord: SkyCoord, radius: Angle) -> List[Dict[str, Any]]:
+    """Perform a cone search for sources with the Transient Name Server (TNS).
+
+    Args:
+        coord: The coordinate of the centre of the cone.
+        radius: The radius of the cone in angular units.
+
+    Returns:
+        A list of dicts, where each dict is a query result row with the following keys:
+
+        - object_name: the name of the transient.
+        - database: the source of the result, i.e. TNS.
+        - separation_arcsec: separation to the query coordinate in arcsec.
+        - otype: object type.
+        - otype_long: long form of the object type. Not given by TNS, will always be
+            an empty string.
+        - ra_hms: RA coordinate string in hms format.
+        - dec_dms: Dec coordinate string in ±dms format.
+    """
+    TNS_API_URL = "https://www.wis-tns.org/api/"
+    headers = {
+        "user-agent": settings.TNS_USER_AGENT,
+    }
+
+    search_dict = {
+        "ra": coord.ra.to_string(unit="hourangle", sep=":", pad=True),
+        "dec": coord.dec.to_string(unit="deg", sep=":", alwayssign=True, pad=True),
+        "radius": str(radius.value),
+        "units": radius.unit.name,
+    }
+    r = requests.post(
+        urljoin(TNS_API_URL, "get/search"),
+        data={"api_key": settings.TNS_API_KEY, "data": json.dumps(search_dict)},
+        headers=headers,
+    )
+    tns_results_dict_list: List[Dict[str, Any]]
+    if r.ok:
+        tns_results_dict_list = r.json()["data"]["reply"]
+        # Get details for each object result. TNS API doesn't support doing this in one
+        # request, so we iterate.
+        for result in tns_results_dict_list:
+            search_dict = {
+                "objname": result["objname"],
+            }
+            r = requests.post(
+                urljoin(TNS_API_URL, "get/object"),
+                data={"api_key": settings.TNS_API_KEY, "data": json.dumps(search_dict)},
+                headers=headers,
+            )
+            if r.ok:
+                object_dict = r.json()["data"]["reply"]
+                object_coord = SkyCoord(
+                    ra=object_dict["radeg"], dec=object_dict["decdeg"], unit="deg"
+                )
+                result["otype"] = object_dict["object_type"]["name"]
+                if result["otype"] is None:
+                    result["otype"] = ""
+                result["otype_long"] = ""
+                result["separation_arcsec"] = coord.separation(object_coord).arcsec
+                result["ra_hms"] = object_coord.ra.to_string(unit="hourangle")
+                result["dec_dms"] = object_coord.dec.to_string(unit="deg")
+                result["database"] = "TNS"
+                result["object_name"] = object_dict["objname"]
+    return tns_results_dict_list
