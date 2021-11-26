0c034a96eb253923f1eeeb32c7ffd8ab2849ab0f
Andrew O'Brien <marxide@users.noreply.github.com>
2021-11-26 11:23:30 -0600

    Clean up m2m related objects when deleting a run (#585)
    * delete images and skyregions when deleting runs
    Fixes #584
    
    * updated changelog
    [skip ci]
    
    * fix changelog typos
    
    Co-authored-by: Adam Stewart <ajstewart@users.noreply.github.com>
    
    * add docstrings
    
    Co-authored-by: Adam Stewart <ajstewart@users.noreply.github.com>

--- vast_pipeline/apps.py
+++ vast_pipeline/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class PipelineConfig(AppConfig):
     """Class representing the configuration for the vast_pipeline app."""
     name = 'vast_pipeline'
+
+    def ready(self) -> None:
+        """Initialization tasks performed as soon as the app registry is populated.
+        See <https://docs.djangoproject.com/en/3.1/ref/applications/#django.apps.AppConfig.ready>.
+        """
+        # import the signals to register them with the application
+        import vast_pipeline.signals  # noqa: F401
