a36d93d85c73d88c75054fd40a44bc9a4e719854
Andrew O'Brien <obrienan@uwm.edu>
2022-04-07 12:47:56 -0500

    Cast graph node labels to str. Fixes #598.

--- vast_pipeline/plots.py
+++ vast_pipeline/plots.py
@@ -196,7 +196,7 @@ def plot_lightcurve(
         )
         node_positions_df["lc_index"] = node_positions_df.index.map(
             {v: k for k, v in lightcurve.id.to_dict().items()}
-        ).values
+        ).values.astype(str)
         node_source = ColumnDataSource(node_positions_df)
         edge_source = ColumnDataSource(candidate_measurement_pairs_df)
 
