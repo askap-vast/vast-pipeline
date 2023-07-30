2cf9970a5130b2e41ace5b593f22b9abc600f22d
Dougal Dobie <ddobie@swin.edu.au>
2023-07-30 13:46:08 +1000

    Only log if long

--- vast_pipeline/pipeline/forced_extraction.py
+++ vast_pipeline/pipeline/forced_extraction.py
@@ -651,12 +651,15 @@ def forced_extraction(
     long_comps = extr_df.loc[extr_df['component_id'].str.len() > 63]
     long_isls = extr_df.loc[extr_df['island_id'].str.len() > 63]
     
-    logger.debug("Entries with long names:")
-    logger.debug(long_names)
-    logger.debug("Entries with long component ids:")
-    logger.debug(long_comps)
-    logger.debug("Entries with long island ids:")
-    logger.debug(long_isls)
+    if len(long_names) > 0:
+        logger.debug("Entries with long names:")
+        logger.debug(long_names)
+    if len(long_comps) > 0:
+        logger.debug("Entries with long component ids:")
+        logger.debug(long_comps)
+    if len(long_isls) > 0:
+        logger.debug("Entries with long island ids:")
+        logger.debug(long_isls)
     extr_df = make_upload_measurements(extr_df)
 
     extr_df = extr_df.rename(columns={'source_tmp_id': 'source'})
