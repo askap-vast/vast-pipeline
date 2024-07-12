a6a7c91a18eff61d52eb1f014a155c035259a3b7
Dougal Dobie <ddobie@users.noreply.github.com>
2024-07-12 11:46:08 +1000

    Implement sensible partitioning (#724)
    * Added calculate_n_partitions function
    
    * Implement calculate_n_partitions_function in parallel_groupby
    
    * Implement calculate_n_partitions function in parallel_get_rms_measurements
    
    * Implement calculate_n_partitions function in calculate_measurement_pair_metrics
    
    * Implement calculate_n_partitions function in parallel_groupby
    
    * Set index
    
    * Missing commit
    
    * Fixed df name
    
    * Fixed df name
    
    * Fixed df name
    
    * Fix partition size var name
    
    * Correct calculate_n_partitions import
    
    * Fixed circular import
    
    * Updated changelog

--- vast_pipeline/utils/utils.py
+++ vast_pipeline/utils/utils.py
@@ -378,3 +378,26 @@ def dict_merge(
 
 def timeStamped(fname, fmt="%Y-%m-%d-%H-%M-%S_{fname}"):
     return datetime.now().strftime(fmt).format(fname=fname)
+
+def calculate_n_partitions(df, n_cpu, partition_size_mb=100):
+    """
+    This function will calculate how many partitions a dataframe should be
+    split into.
+    
+    Args:
+        df: The pandas dataframe to be partitionined.
+        n_cpu: The number of available CPUs.
+        partition_size: The optimal partition size in MB.
+    Returns:
+        The optimal number of partitions.
+    """
+    mem_usage_mb = df.memory_usage(deep=True).sum() / 1e6
+    n_partitions = int(np.ceil(mem_usage_mb/partition_size_mb))
+    
+    # n_partitions should be >= n_cpu for optimal parallel processing
+    if n_partitions < n_cpu:
+        n_partitions=n_cpu
+    
+    logger.debug("Using {n_partitions} partions of {partition_size}MB")
+
+    return n_partitions
