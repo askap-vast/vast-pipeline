2fb4320c2bd2e79023ece8b377db8679d893cf39
Dougal Dobie <ddobie@swin.edu.au>
2024-07-18 12:33:11 +1000

    Previous version passes tests locally - reverting to original to check what's going wrong

--- vast_pipeline/pipeline/new_sources.py
+++ vast_pipeline/pipeline/new_sources.py
@@ -232,14 +232,14 @@ def parallel_get_rms_measurements(
         ).compute(num_workers=n_cpu, scheduler='processes')
     )
 
-    """
+    #"""
     df = df.merge(
         out[['source', 'img_diff_true_rms']],
         left_on='source', right_on='source',
         how='left'
     )
-    """
     #"""
+    """
     
     df_to_merge = (df.sort_values(
                         by=['source', 'flux_peak'],
@@ -259,7 +259,7 @@ def parallel_get_rms_measurements(
         left_on='source', right_on='source',
         how='left'
     )
-    #"""
+    """
 
     return df
 
