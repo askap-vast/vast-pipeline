9783865fff5fefc8f9a29ee67b14c6bd8080b6c9
Dougal Dobie <ddobie@swin.edu.au>
2024-09-20 15:52:01 +1000

    Reenabled logging

--- vast_pipeline/utils/delete_run.py
+++ vast_pipeline/utils/delete_run.py
@@ -25,8 +25,8 @@ def delete_pipeline_run_raw_sql(p_run):
         #_run_raw_sql(sql_cmd, cursor)
         
         # Disable triggers
-        sql_cmd = "ALTER TABLE vast_pipeline_source DISABLE TRIGGER ALL;"
-        _run_raw_sql(sql_cmd, cursor)
+        #isql_cmd = "ALTER TABLE vast_pipeline_source DISABLE TRIGGER ALL;"
+        #_run_raw_sql(sql_cmd, cursor)
         
         # Fetch source IDs associated with the pipeline run
         sql_cmd = f"SELECT id FROM vast_pipeline_source WHERE run_id = {p_run_id};"
@@ -40,38 +40,38 @@ def delete_pipeline_run_raw_sql(p_run):
 
             # Delete entries from vast_pipeline_sourcefav for each source_id
             sql_cmd = f"DELETE FROM vast_pipeline_sourcefav WHERE source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
 
             # Find source tags related to the source and delete them
             sql_cmd = f"SELECT tagulous_source_tags_id FROM vast_pipeline_source_tags WHERE source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
             tagulous_source_tags_ids = cursor.fetchall()
 
             for tagulous_source_tags_id_tuple in tagulous_source_tags_ids:
                 tagulous_source_tags_id = tagulous_source_tags_id_tuple[0]
                 sql_cmd = f"DELETE FROM vast_pipeline_tagulous_source_tags WHERE id = {tagulous_source_tags_id};"
-                _run_raw_sql(sql_cmd, cursor, log=False)
+                _run_raw_sql(sql_cmd, cursor, log=True)
 
             # Delete from vast_pipeline_source_tags for the source_id
             sql_cmd = f"DELETE FROM vast_pipeline_source_tags WHERE source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
 
             # Delete from related source
             sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE from_source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
             sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE to_source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
 
             sql_cmd = f"DELETE FROM vast_pipeline_association WHERE source_id = {source_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
 
         # Delete source
         sql_cmd = f"DELETE FROM vast_pipeline_source WHERE run_id = {p_run_id};"
         _run_raw_sql(sql_cmd, cursor)
         
         # Enable triggers
-        sql_cmd = "ALTER TABLE vast_pipeline_source ENABLE TRIGGER ALL;"
-        _run_raw_sql(sql_cmd, cursor)
+        #sql_cmd = "ALTER TABLE vast_pipeline_source ENABLE TRIGGER ALL;"
+        #_run_raw_sql(sql_cmd, cursor)
 
         # Delete comments
         sql_cmd = f"DELETE FROM vast_pipeline_comment WHERE object_id = {p_run_id};"
@@ -88,17 +88,17 @@ def delete_pipeline_run_raw_sql(p_run):
             image_id = image_id_tuple[0]
             try:
                 sql_cmd = f"DELETE FROM vast_pipeline_measurement WHERE image_id = {image_id};"
-                _run_raw_sql(sql_cmd, cursor, log=False)
+                _run_raw_sql(sql_cmd, cursor, log=True)
             except Exception as e:
                 print("++++++++++++++++++++++++++++++++")
                 print(e, image_id)
                 print("++++++++++++++++++++++++++++++++")
                 pass
             sql_cmd = f"DELETE FROM vast_pipeline_image_run WHERE image_id = {image_id} AND run_id = {p_run_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
             try:
                 sql_cmd = f"DELETE FROM vast_pipeline_image WHERE id = {image_id};"
-                _run_raw_sql(sql_cmd, cursor, log=False)
+                _run_raw_sql(sql_cmd, cursor, log=True)
             except Exception as e:
                 print("++++++++++++++++++++++++++++++++")
                 print(e, image_id)
@@ -115,10 +115,10 @@ def delete_pipeline_run_raw_sql(p_run):
         for sky_id_tuple in tqdm(sky_ids):
             sky_id = sky_id_tuple[0]
             sql_cmd = f"DELETE FROM vast_pipeline_skyregion_run WHERE skyregion_id = {sky_id} AND run_id = {p_run_id};"
-            _run_raw_sql(sql_cmd, cursor, log=False)
+            _run_raw_sql(sql_cmd, cursor, log=True)
             try:
                 sql_cmd = f"DELETE FROM vast_pipeline_skyregion WHERE id = {sky_id};"
-                _run_raw_sql(sql_cmd, cursor, log=False)
+                _run_raw_sql(sql_cmd, cursor, log=True)
             except Exception as e:
                 print("++++++++++++++++++++++++++++++++")
                 print(e, sky_id)
