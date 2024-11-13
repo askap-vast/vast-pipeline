d095377a7b58568440616c48cc1f0ec97d3cc6cd
Dougal Dobie <ddobie@swin.edu.au>
2024-11-13 16:56:40 +1100

    Converted vast_pipeline/management/helpers.py

--- vast_pipeline/management/helpers.py
+++ vast_pipeline/management/helpers.py
@@ -2,7 +2,7 @@
 Helper functions for the commands.
 """
 
-import os
+from pathlib import Path
 import logging
 
 from typing import Tuple
@@ -26,10 +26,11 @@ def get_p_run_name(name: str, return_folder: bool = False) -> Tuple[str, str]:
         The run directory (if `return_folder` is set to `True`).
     """
     if '/' in name:
-        folder = os.path.realpath(name)
-        run_name = os.path.basename(folder)
+        folder = Path(name).resolve()
+        run_name = folder.parent[0]
         return (run_name, folder) if return_folder else run_name
 
-    folder = os.path.join(os.path.realpath(sett.PIPELINE_WORKING_DIR), name)
+    working_dir = Path(sett.PIPELINE_WORKING_DIR).resolve()
+    folder = working_dir/name
 
     return (name, folder) if return_folder else name
