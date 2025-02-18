9c3b4586594aa9a4c3cc0793a2bdd9aa6841b156
Tom Mauch <thomas.mauch@sydney.edu.au>
2025-02-18 17:21:57 +1100

    Fix NULL related sources in association.py

--- vast_pipeline/pipeline/association.py
+++ vast_pipeline/pipeline/association.py
@@ -1286,8 +1286,10 @@ def association(
     # Finally the related column Null entries are filled with a list containing "NULL"
     # to avoid dask schema issues later on.
     related_null_mask = sources_df["related"].isnull()
-    sources_df.loc[related_null_mask, "related"] = ["NULL",]
-
+    sources_df.loc[related_null_mask, "related"] = "NULL"
+    sources_df.loc[related_null_mask, "related"] = sources_df.loc[related_null_mask, "related"].apply(
+        lambda x: [x,]
+    )
     logger.info(
         "Total association time: %.2f seconds%s.", timer.reset_init(), skyreg_tag
     )
