ee7a87056baa0e6d112d2565b2cbf28214ab96ff
Dougal Dobie <ddobie@users.noreply.github.com>
2025-05-14 18:01:29 +1000

    Fix syntax error

--- vast_pipeline/image/main.py
+++ vast_pipeline/image/main.py
@@ -18,13 +18,10 @@ from .utils import calc_condon_flux_errors, open_fits
 
 from vast_pipeline import models
 from vast_pipeline.survey.translators import tr_selavy
-<<<<<<< HEAD
 from vast_pipeline.utils.utils import (
     generate_shortuuid, UUID_LEN_MEAS,
 )
-=======
 from vast_pipeline.image.utils import get_fits_header
->>>>>>> dev
 
 
 logger = logging.getLogger(__name__)
