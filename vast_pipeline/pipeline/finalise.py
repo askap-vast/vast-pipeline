d9fa83a2b42cbf2efbe0075cd940ea375af61524
Dougal Dobie <ddobie@swin.edu.au>
2025-05-20 18:24:02 +1000

    PEP8

--- vast_pipeline/pipeline/finalise.py
+++ vast_pipeline/pipeline/finalise.py
@@ -11,7 +11,6 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 from django.conf import settings
 from typing import List, Dict, Tuple
-from dask.distributed import wait
 
 from vast_pipeline.models import Run
 from vast_pipeline.utils.utils import (
