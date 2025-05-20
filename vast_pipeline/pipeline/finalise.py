107b29bbd72eb70ed29460c2b77fcb6acba92ed2
Dougal Dobie <ddobie@swin.edu.au>
2025-05-20 17:10:55 +1000

    Fix duplicate source ID issue

--- vast_pipeline/pipeline/finalise.py
+++ vast_pipeline/pipeline/finalise.py
@@ -96,8 +96,15 @@ def final_operations(
     logger.info("Calculating statistics for sources...")
     log_total_memory_usage()
 
-    sources_df = sources_df.set_index("source") \
-                           .repartition(partition_size=f"{upload_chunk_size_mb}MB")
+    npartitions = calculate_n_partitions(
+        sources_df,
+        partition_size_mb=upload_chunk_size_mb
+    )
+
+    with dc.set({"dataframe.shuffle.method": "p2p"}):
+        sources_df = sources_df.set_index("source") \
+                               .shuffle(npartitions=npartitions, on_index=True)
+
     srcs_df = parallel_groupby(sources_df)
 
     mem_usage = get_df_memory_usage(srcs_df)
