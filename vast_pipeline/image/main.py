99ce53f23a6766985adfe6e1c88410175ac66cd0
Dougal Dobie <ddobie@swin.edu.au>
2025-08-28 17:28:13 +1000

    Fix uncertainty_ew/ns calculation

--- vast_pipeline/image/main.py
+++ vast_pipeline/image/main.py
@@ -438,11 +438,11 @@ class SelavyImage(FitsImage):
         )
 
         df['uncertainty_ew'] = np.hypot(
-            df['ew_sys_err'].values, df['error_radius'].values
+            df['ew_sys_err'].values, df['ra_err'].values
         )
 
         df['uncertainty_ns'] = np.hypot(
-            df['ns_sys_err'].values, df['error_radius'].values
+            df['ns_sys_err'].values, df['dec_err'].values
         )
 
         # weight calculations to use later
