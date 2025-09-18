8ae5a6e20d9eb1f07ccc91b01fc590fc7d5b8333
Dougal Dobie <ddobie@swin.edu.au>
2025-09-18 15:16:43 +1000

    Improve CLI for cleanup

--- vast_pipeline/management/commands/cleanup_cutouts.py
+++ vast_pipeline/management/commands/cleanup_cutouts.py
@@ -6,10 +6,12 @@ from django.db import transaction
 from django.conf import settings
 from vast_pipeline.models import ImageCutout
 
+from argparse import ArgumentParser
+
 
 CUTOFF_DAYS = settings.CUTOFF_DAYS
 MAX_SIZE_GB = settings.MAX_SIZE_GB
-CUTOUT_DIR = settings.CUTOUT_DIR
+CUTOUTS_PATH = settings.MEDIA_ROOT
 
 
 class Command(BaseCommand):
@@ -45,19 +47,49 @@ class Command(BaseCommand):
 
         return file_size
 
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        """
+        Enables arguments for the command.
+
+        Args:
+            parser (ArgumentParser): The parser object of the command.
+
+        Returns:
+            None
+        """
+        # positional arguments
+        parser.add_argument(
+            '--cutoff-days',
+            type=int,
+            required=False,
+            help="Remove data older than this number of days. Default to CUTOFF_DAYS provided in settings",
+            default=CUTOFF_DAYS
+        )
+
+        parser.add_argument(
+            '--max-size-gb',
+            type=float,
+            required=False,
+            default=MAX_SIZE_GB,
+            help="Maximum size of the cache in GB. Default to MAX_SIZE_GB provided in settings.",
+        )
+
     def handle(self, *args, **kwargs):
 
-        if not CUTOUT_DIR:
-            self.stdout.write("CUTOUT_DIR is not set. Aborting cleanup.")
+        if not CUTOUTS_PATH:
+            self.stdout.write("CUTOUTS_PATH is not set. Aborting cleanup.")
             return
 
+        cutoff_days = kwargs['cutoff_days']
+        max_size_gb = kwargs['max_size_gb']
+        
         now = timezone.now()
 
         # delete based on age
-        total_bytes = self.get_dir_size(CUTOUT_DIR)
-        threshold = now - timedelta(days=CUTOFF_DAYS)
+        total_bytes = self.get_dir_size(CUTOUTS_PATH)
+        threshold = now - timedelta(days=cutoff_days)
         old_cutouts = ImageCutout.objects.filter(last_accessed__lt=threshold)
-        self.stdout.write(f"Deleting {old_cutouts.count()} cutouts older than {CUTOFF_DAYS} days.")
+        self.stdout.write(f"Deleting {old_cutouts.count()} cutouts older than {cutoff_days} days.")
         for cutout in old_cutouts:
             reclaimed_size_bytes = self.delete_cutout(cutout)
             total_bytes -= reclaimed_size_bytes
@@ -66,15 +98,15 @@ class Command(BaseCommand):
         total_gb = total_bytes / (1024 ** 3)
         self.stdout.write(f"Current cutout dir size: {total_gb:.2f} GB")
 
-        if total_gb > MAX_SIZE_GB:
-            self.stdout.write(f"Cutout size exceeds {MAX_SIZE_GB}GB. Trimming...")
+        if total_gb > max_size_gb:
+            self.stdout.write(f"Cutout size exceeds {max_size_gb}GB. Trimming...")
             # sorting based on oldest accessed
             cutouts = ImageCutout.objects.order_by('last_accessed')
             for cutout in cutouts:
                 reclaimed_size_bytes = self.delete_cutout(cutout)
                 total_bytes -= reclaimed_size_bytes
                 total_gb = total_bytes / (1024 ** 3)
-                if total_gb <= MAX_SIZE_GB:
+                if total_gb <= max_size_gb:
                     self.stdout.write(f"Trimmed down to {total_gb:.2f} GB.")
                     break
         else:
