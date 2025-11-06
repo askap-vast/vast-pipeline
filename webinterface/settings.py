5dbc8f14605a557a0639ce8e4c60bd0ba851ea8b
Dougal Dobie <ddobie@swin.edu.au>
2025-11-06 12:50:47 +1100

    Update default aladin search radius to be 0.6 arcmin

--- webinterface/settings.py
+++ webinterface/settings.py
@@ -362,4 +362,4 @@ MAX_SIZE_GB = env("MAX_SIZE_GB", cast=float, default=0.01)
 MEDIA_ROOT = env("MEDIA_ROOT", cast=str, default=None)
 
 ALADIN_ZOOM = env("ALADIN_ZOOM", cast=float, default=0.017)
-ALADIN_RADIUS = env("ALADIN_RADIUS", cast=float, default=1)
+ALADIN_RADIUS = env("ALADIN_RADIUS", cast=float, default=0.6)
