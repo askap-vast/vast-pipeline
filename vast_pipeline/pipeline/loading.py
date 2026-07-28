askap-vast/vast-pipeline

commit 07b039f7eb06fb09983db116f9faf0435798a015

Run deletion optimisation (#840)
* Implement batching

* Added tqdm logging

* Added flag to keep images

* Minor cleanup

* More tweaks

* Fixed forced extraction code

* Clean up skyregion code

* Initial cleanup

* Clean up forced_extraction

* Docstring

* PEP8

* I think I've fixed all of the foreign key issues?

* Clean up test_clearpiperun

* Clean up delete_run

* Updated changelog

* Convert to _clear_old_sources function

* Refactor delete_run

* add finish statement to clearpiperun

* Updated changelog

* Fix missing timer?

* Loading changes

* Converted back to django to avoid foreign key issues

* PEP8 nonsense

* PEP8

* Update vast_pipeline/management/commands/clearpiperun.py

Co-authored-by: Tom Mauch <thomas.mauch@sydney.edu.au>

* Update vast_pipeline/pipeline/loading.py

Co-authored-by: Tom Mauch <thomas.mauch@sydney.edu.au>

* Make batch optional arg

* Add v2 upgrade note

* Fixed syntax error

---------

Co-authored-by: Tom Mauch <thomas.mauch@sydney.edu.au>

vast_pipeline/pipeline/loading.py
@@ -95,8 +95,11 @@ def make_upload_images(
     images = []
     skyregions = []
     bands = []
+    
+    dt = StopWatch()
 
     for path in paths['selavy']:
+        dt.reset()
         # STEP #1: Load image and measurements
         image = SelavyImage(
             path,
@@ -104,22 +107,26 @@ def make_upload_images(
             image_config
         )
         logger.info('Reading image %s ...', image.name)
+        logger.debug('Generated SelavyImage in %.3f s', dt.reset())
 
         # 1.1 get/create the frequency band
         with transaction.atomic():
             band = get_create_img_band(image)
         if band not in bands:
             bands.append(band)
+        logger.debug('Generated band in %.3f s', dt.reset())
 
         # 1.2 create image and skyregion entry in DB
         with transaction.atomic():
             img, exists_f = get_create_img(band.id, image)
+            logger.debug('get_create_img in %.3f s', dt.reset())
             skyreg = img.skyreg
 
             # add image and skyregion to respective lists
             images.append(img)
             if skyreg not in skyregions:
                 skyregions.append(skyreg)
+            logger.debug('Images and skyregions appended in %.3f s', dt.reset())
 
             if exists_f:
                 logger.info("Image %s already processed", img.name)
@@ -153,7 +160,10 @@ def make_upload_images(
 
 
 def make_upload_sources(
-    sources_df: pd.DataFrame, pipeline_run: Run, add_mode: bool = False
+    sources_df: pd.DataFrame,
+    pipeline_run: Run,
+    add_mode: bool = False,
+    batch_size: int = 1000
 ) -> pd.DataFrame:
     '''
     Delete previous sources for given pipeline run and bulk upload
@@ -161,12 +171,14 @@ def make_upload_sources(
 
     Args:
         sources_df:
-            Holds the measurements associated into sources. The output of of
-            thE association step.
+            Holds the measurements associated into sources. The output of
+            the association step.
         pipeline_run:
             The pipeline Run object.
         add_mode:
-            Whether the pipeline is running in add image mode.
+            Whether the pipeline is running in add image mode. Defaults to False.
+        batch_size:
+            The size of batch to use. Defaults to 1000.
 
     Returns:
         The input dataframe with the 'id' column added.
@@ -177,21 +189,32 @@ def make_upload_sources(
     logger.debug(f"sources_df memory usage: {mem_usage}MB")
     log_total_memory_usage()
 
-    # create sources in DB
+    # NOTE - all of this logic will need to be updated for V2 changes
     with transaction.atomic():
-        if (add_mode is False and
-                Source.objects.filter(run=pipeline_run).exists()):
-            logger.info('Removing objects from previous pipeline run')
-            n_del, detail_del = (
-                Source.objects.filter(run=pipeline_run).delete()
-            )
-            logger.info(
-                ('Deleting all sources and related objects for this run. '
-                 'Total objects deleted: %i'),
-                n_del,
-            )
-            logger.debug('(type, #deleted): %s', detail_del)
+        sources = Source.objects.filter(run=pipeline_run)
+        if not add_mode and sources.exists():
+            logger.info('Removing objects from previous pipeline run...')
 
+            ids_to_delete = list(sources.values_list('id', flat=True))
+            total_deleted = 0
+
+            for i in range(0, len(source_ids), batch_size):
+                batch_ids = source_ids[i : i + batch_size]
+                n_deleted, details = Source.objects.filter(id__in=batch_ids).delete()
+                total_deleted += n_deleted
+
+                logger.debug(
+                    "Deleted %d objects in this batch.", n_deleted
+                )
+
+            logger.info(
+                'Deleting all sources and related objects for this run. '
+                'Total objects deleted: %i',
+                total_deleted
+            )
+    
+    
+    # create sources in DB
     src_dj_ids = bulk_upload_model(
         Source,
         source_models_generator(sources_df, pipeline_run=pipeline_run),

vast_pipeline/management/commands/clearpiperun.py
@@ -67,6 +67,21 @@ class Command(BaseCommand):
             action='store_true',
             help='Flag to remove all the content of the pipeline run(s) folder.'
         )
+        parser.add_argument(
+            '--delete-images',
+            required=False,
+            default=False,
+            action='store_true',
+            help='Flag to delete all images associated with the run.'
+        )
+        parser.add_argument(
+            '--source-batch-size',
+            required=False,
+            default=10000,
+            type=int,
+            help='Batch size to use for source deletion.'
+        )
+        
 
     def handle(self, *args, **options) -> None:
         """
@@ -113,7 +128,10 @@ class Command(BaseCommand):
                 p_run.save()
 
             timer.reset()
-            delete_pipeline_run_raw_sql(p_run)
+            delete_pipeline_run_raw_sql(
+                p_run,
+                delete_images=options['delete_images'],
+                source_batch_size=options['source_batch_size'])
             t = timer.reset()
             logger.info("Time to delete run from database: %.2f sec", t)
 
@@ -137,8 +155,8 @@ class Command(BaseCommand):
                             f'Parquet file "{os.path.basename(parquet)}" not existent'
                         ))
                         pass
-            t = timer.reset()
-            logger.info("Time to delete parquet files: %.2f sec", t)
+                t = timer.reset()
+                logger.info("Time to delete parquet files: %.2f sec", t)
 
             if options['remove_all']:
                 logger.info('Deleting pipeline folder')
@@ -149,3 +167,5 @@ class Command(BaseCommand):
                         f'Issues in removing run folder: {e}'
                     ))
                     pass
+
+            logger.info(f"Successfully cleared {p_run_name}")

vast_pipeline/pipeline/forced_extraction.py
@@ -10,11 +10,10 @@ from glob import glob
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from django.conf import settings
-from django.db import transaction
 from pyarrow.parquet import read_schema
 from typing import Any, List, Tuple, Dict, Optional
 
-from vast_pipeline.models import Image, Measurement, Run
+from vast_pipeline.models import Image, Run
 from vast_pipeline.pipeline.loading import make_upload_measurements
 
 from forced_phot import ForcedPhot
@@ -25,7 +24,7 @@ from vast_pipeline.pipeline.utils import log_total_memory_usage
 logger = logging.getLogger(__name__)
 
 
-def remove_forced_meas(run_path: str) -> None:
+def remove_forced_meas(run_path: str, batch_size=10000) -> None:
     '''
     Remove forced measurements from the database if forced parquet files
     are found.
@@ -33,6 +32,8 @@ def remove_forced_meas(run_path: str) -> None:
     Args:
         run_path:
             The run path of the pipeline run.
+        batch_size:
+            Number of forced measurements to delete per iteration
 
     Returns:
         None
@@ -47,17 +48,26 @@ def remove_forced_meas(run_path: str) -> None:
             .compute()
             .tolist()
         )
-        obj_to_delete = Measurement.objects.filter(id__in=ids)
-        del ids
-        if obj_to_delete.exists():
+
+        forced_meas = Measurement.objects.filter(id__in=ids)
+        if forced_meas.exists():
             with transaction.atomic():
-                n_del, detail_del = obj_to_delete.delete()
+                logger.info("Iterating over %d forced measurements", len(forced_meas))
+                n_batches = round(n_ids/batch_size)
+                logger.info("Using %d batches of %d measurements", n_batches, batch_size)
+
+                ids_to_delete = list(forced_meas.values_list('id', flat=True))
+                total_deleted = 0
+
+                for i in range(0, len(source_ids), BATCH_SIZE):
+                    batch_ids = ids_to_delete[i : i + BATCH_SIZE]
+                    n_deleted, details = Measurement.objects.filter(id__in=batch_ids).delete()
+                    total_deleted += n_deleted
+
                 logger.info(
-                    ('Deleting all previous forced measurement and association'
-                     ' objects for this run. Total objects deleted: %i'),
-                    n_del,
+                    'Total objects deleted: %i',
+                    total_deleted
                 )
-                logger.debug('(type, #deleted): %s', detail_del)
 
 
 def get_data_from_parquet(

vast_pipeline/tests/test_clearpiperun.py
@@ -2,7 +2,7 @@ import os
 from typing import Dict
 
 from django.conf import settings as s
-from django.test import TestCase, override_settings
+from django.test import TransactionTestCase, override_settings
 from django.utils import timezone
 from django.core.management import call_command
 
@@ -15,15 +15,15 @@ TEST_ROOT = os.path.join(s.BASE_DIR, "vast_pipeline", "tests")
 @override_settings(
     PIPELINE_WORKING_DIR=os.path.join(TEST_ROOT, "pipeline-runs"),
 )
-class ClearPipelineRunTest(TestCase):
+class ClearPipelineRunTest(TransactionTestCase):
     band: Band
     run_1: Run
     run_2: Run
     skyregs: Dict[str, SkyRegion]
     images: Dict[str, Image]
 
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(self):
+
         default_image_kwargs = dict(
             measurements_path="",
             polarisation="I",
@@ -63,41 +63,41 @@ class ClearPipelineRunTest(TestCase):
         # The letter suffix indicates the sky region, e.g. image_a1 is in sky region a.
 
         # create a band
-        cls.band = Band.objects.create(name="band", frequency=887, bandwidth=128)
+        self.band = Band.objects.create(name="band", frequency=887, bandwidth=128)
         # create runs
-        cls.run_1 = Run.objects.create(name="run_1", path="run_1")
-        cls.run_2 = Run.objects.create(name="run_2", path="run_2")
+        self.run_1 = Run.objects.create(name="run_1", path="run_1")
+        self.run_2 = Run.objects.create(name="run_2", path="run_2")
         # create sky regions
-        cls.skyregs = {
+        self.skyregs = {
             k: SkyRegion.objects.create(**default_skyreg_kwargs)
             for k in ("a", "b", "c")
         }
         # create some images
-        cls.images = {
+        self.images = {
             f"{k}1": Image.objects.create(
-                band=cls.band,
+                band=self.band,
                 skyreg=skyreg,
                 name=f"image_{k}1",
                 **default_image_kwargs,
             )
-            for k, skyreg in cls.skyregs.items()
+            for k, skyreg in self.skyregs.items()
         }
-        cls.images["a2"] = Image.objects.create(
-            band=cls.band,
-            skyreg=cls.skyregs["a"],
+        self.images["a2"] = Image.objects.create(
+            band=self.band,
+            skyreg=self.skyregs["a"],
             name="image_a2",
             **default_image_kwargs,
         )
-        cls.images["a1"].run.add(cls.run_1)
-        cls.images["a2"].run.add(cls.run_1)
-        cls.images["b1"].run.add(cls.run_1)
-        cls.images["b1"].run.add(cls.run_2)
-        cls.images["c1"].run.add(cls.run_2)
+        self.images["a1"].run.add(self.run_1)
+        self.images["a2"].run.add(self.run_1)
+        self.images["b1"].run.add(self.run_1)
+        self.images["b1"].run.add(self.run_2)
+        self.images["c1"].run.add(self.run_2)
 
-        cls.skyregs["a"].run.add(cls.run_1)
-        cls.skyregs["b"].run.add(cls.run_1)
-        cls.skyregs["b"].run.add(cls.run_2)
-        cls.skyregs["c"].run.add(cls.run_2)
+        self.skyregs["a"].run.add(self.run_1)
+        self.skyregs["b"].run.add(self.run_1)
+        self.skyregs["b"].run.add(self.run_2)
+        self.skyregs["c"].run.add(self.run_2)
 
     def test_data(self):
         """Check that the object relationships exist as expected.
@@ -112,7 +112,7 @@ class ClearPipelineRunTest(TestCase):
         """Delete a run and check that the appropriate objects are also deleted.
         """
         # delete run_1
-        call_command("clearpiperun", self.run_1.name)
+        call_command("clearpiperun", self.run_1.name, delete_images=True)
 
         # ensure the images and sky regions that were only used in run_1 are deleted
         with self.assertRaises(Image.DoesNotExist):
@@ -121,12 +121,13 @@ class ClearPipelineRunTest(TestCase):
             _ = Image.objects.get(name="image_a2")
         with self.assertRaises(SkyRegion.DoesNotExist):
             _ = SkyRegion.objects.get(pk=self.skyregs["a"].pk)
+
         # ensure the images and sky regions that were used in both run_1 and run_2 remain
         _ = Image.objects.get(name="image_b1")
         _ = SkyRegion.objects.get(pk=self.skyregs["b"].pk)
 
         # delete run_2
-        call_command("clearpiperun", self.run_2.name)
+        call_command("clearpiperun", self.run_2.name, delete_images=True)
         # ensure no images nor sky regions remain
         self.assertEqual(Image.objects.count(), 0)
         self.assertEqual(SkyRegion.objects.count(), 0)

vast_pipeline/utils/delete_run.py
@@ -1,29 +1,145 @@
 import logging
 
+from tqdm import tqdm
 from django.db import connection
 from vast_pipeline.utils.utils import StopWatch
 
 logger = logging.getLogger(__name__)
 
-def _run_raw_sql(command, cursor, debug=False, log=True):
+def _run_raw_sql(command, cursor, debug=False, log=True, dry_run=False):
     if log:
         if debug:
             logger.debug("Running %s", command)
         else:
             logger.info("Running %s", command)
-    cursor.execute(command)
+    if not dry_run:
+        cursor.execute(command)
     
     return
 
-def delete_pipeline_run_raw_sql(p_run):
+def delete_pipeline_run_raw_sql(p_run, source_batch_size=10000, delete_images=False):
     p_run_id = p_run.pk
 
     with connection.cursor() as cursor:
-        # Disable triggers
-        #sql_cmd = "ALTER TABLE vast_pipeline_source DISABLE TRIGGER ALL;"
-        #_run_raw_sql(sql_cmd, cursor)
+        #####
+        #
+        # Step 1: handle sources
+        #
+        #####
+        timer = StopWatch()
+        clear_run_sources(p_run_id, batch_size=source_batch_size, timer=timer)
         
-        # Fetch source IDs associated with the pipeline run
+        #####
+        #
+        # Step 2: Handle images
+        #
+        #####
+
+        # Fetch image IDs associated with the pipeline run
+        timer.reset()
+        sql_cmd = f"SELECT image_id FROM vast_pipeline_image_run WHERE run_id = {p_run_id};"
+        _run_raw_sql(sql_cmd, cursor)
+        image_ids = cursor.fetchall()
+        n_image_ids = len(image_ids)
+        image_id_str = ','.join(str(image_id[0]) for image_id in image_ids)
+        logger.debug(f"images associated with run: {image_id_str}")
+        
+        if n_image_ids > 0:
+            # Only delete the actual images if selected, but only delete if they're not linked to other runs
+            if delete_images:
+                # Get the images that are uniquely associated with the pipeline run
+                sql_cmd = f"SELECT image_id FROM vast_pipeline_image_run GROUP BY image_id HAVING COUNT(*) = 1 AND MAX(run_id) = {p_run_id};"
+                _run_raw_sql(sql_cmd, cursor)
+                unique_image_ids = cursor.fetchall()
+                unique_image_id_str = ','.join(str(image_id[0]) for image_id in unique_image_ids)
+                logger.debug(f"Iterating over image_ids unique to run: {unique_image_id_str}")
+
+                # ...and delete the measurements associated with them
+                for image_id_tuple in unique_image_ids:
+                    image_id = image_id_tuple[0]
+                    try:
+                        sql_cmd = f"DELETE FROM vast_pipeline_measurement WHERE image_id = {image_id};"
+                        _run_raw_sql(sql_cmd, cursor)
+                    except Exception as e:
+                        logger.error("%s %d", e, image_id)
+                        pass
+
+            # Delete link between run and images for all images
+            sql_cmd = f"DELETE FROM vast_pipeline_image_run WHERE run_id = {p_run_id} AND image_id IN ({image_id_str});"
+            _run_raw_sql(sql_cmd, cursor)
+
+            # Delete images that are uniquely associated with the run
+            if delete_images:
+                if len(unique_image_ids) > 0:
+                    try:
+                        sql_cmd = f"DELETE FROM vast_pipeline_image WHERE id IN ({unique_image_id_str});"
+                        _run_raw_sql(sql_cmd, cursor)
+                    except Exception as e:
+                        logger.error("%s %d", e, image_id_str)
+                        pass
+
+            
+            t = timer.reset()
+            logger.info("Time to iterate over %d image ids: %f seconds", n_image_ids, t)
+        else:
+            logger.info("No images to delete...")
+
+                
+        #####
+        #
+        # Step 3: Handle skyregions
+        #
+        #####
+
+        # Fetch skyregion IDs associated with the pipeline run
+        sql_cmd = f"SELECT skyregion_id FROM vast_pipeline_skyregion_run WHERE run_id = {p_run_id};"
+        _run_raw_sql(sql_cmd, cursor, debug=True)
+        sky_ids = cursor.fetchall()
+        
+        # Delete all rows from vast_pipeline_skyregion_run for the run:
+        sql_cmd = f"DELETE FROM vast_pipeline_skyregion_run WHERE run_id = {p_run_id};"
+        _run_raw_sql(sql_cmd, cursor)
+        
+        for sky_id_tuple in sky_ids:
+            sky_id = sky_id_tuple[0]
+            sql_cmd = f"SELECT COUNT(*) FROM vast_pipeline_image WHERE skyreg_id = {sky_id};"
+            _run_raw_sql(sql_cmd, cursor)
+            
+            image_count = cursor.fetchone()[0]
+            # Delete skyregions that no longer have an image associated with them
+            if image_count > 0:
+                logger.debug("Not deleting skyregion_id %d; %d image(s) still reference it.", sky_id, image_count)
+                continue
+            else:
+                sql_cmd = f"DELETE FROM vast_pipeline_skyregion WHERE id = {sky_id};"
+                _run_raw_sql(sql_cmd, cursor)
+
+        #####
+        #
+        # Step 4: Final cleanup steps
+        #
+        #####
+
+        # Delete comments
+        sql_cmd = f"DELETE FROM vast_pipeline_comment WHERE object_id = {p_run_id};"
+        _run_raw_sql(sql_cmd, cursor)
+
+        # Finally delete the pipeline run
+        sql_cmd = f"DELETE FROM vast_pipeline_run WHERE id = {p_run_id};"
+        _run_raw_sql(sql_cmd, cursor)
+
+def clear_run_sources(p_run_id, batch_size=10_000, timer=None):
+    """
+    Clear the existing sources associated with a run
+    
+    Args:
+        run: the pipeline run to clear
+    """
+
+    if timer is None:
+        timer = StopWatch()
+
+    with connection.cursor() as cursor:
         sql_cmd = f"SELECT id FROM vast_pipeline_source WHERE run_id = {p_run_id};"
         _run_raw_sql(sql_cmd, cursor)
         source_ids = cursor.fetchall()
@@ -31,131 +147,48 @@ def delete_pipeline_run_raw_sql(p_run):
         # Iterate over each source ID and delete related information
         n_source_ids = len(source_ids)
         logger.info("Iterating over %d sources to delete tags and relations", n_source_ids)
-        timer = StopWatch()
-        for i, source_id_tuple in enumerate(source_ids):
-            source_id = source_id_tuple[0]
+        n_batches = round(n_source_ids/batch_size)
+        logger.info("Using %d batches of %d sources", n_batches, batch_size)
+
+        batch_starts = list(range(0, len(source_ids), batch_size))
+        timer.reset()
+        for batch_start in tqdm(batch_starts):
+            batch = source_ids[batch_start:batch_start+batch_size]
+            batch_str = ','.join(str(source_id[0]) for source_id in batch)
 
             # Delete entries from vast_pipeline_sourcefav for each source_id
-            sql_cmd = f"DELETE FROM vast_pipeline_sourcefav WHERE source_id = {source_id};"
+            sql_cmd = f"DELETE FROM vast_pipeline_sourcefav WHERE source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
 
             # Find source tags related to the source and delete them
-            sql_cmd = f"SELECT tagulous_source_tags_id FROM vast_pipeline_source_tags WHERE source_id = {source_id};"
+            sql_cmd = f"SELECT tagulous_source_tags_id FROM vast_pipeline_source_tags WHERE source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
-            tagulous_source_tags_ids = cursor.fetchall()
-
-            for tagulous_source_tags_id_tuple in tagulous_source_tags_ids:
-                tagulous_source_tags_id = tagulous_source_tags_id_tuple[0]
-                sql_cmd = f"DELETE FROM vast_pipeline_tagulous_source_tags WHERE id = {tagulous_source_tags_id};"
+            tag_ids = cursor.fetchall()
+            if tag_ids:
+                tag_ids_str = ','.join(str(t[0]) for t in tag_ids)
+                sql_cmd = f"DELETE FROM vast_pipeline_tagulous_source_tags WHERE id IN ({tag_ids_str});"
                 _run_raw_sql(sql_cmd, cursor, log=False)
 
             # Delete from vast_pipeline_source_tags for the source_id
-            sql_cmd = f"DELETE FROM vast_pipeline_source_tags WHERE source_id = {source_id};"
+            sql_cmd = f"DELETE FROM vast_pipeline_source_tags WHERE source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
 
             # Delete from related source
-            sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE from_source_id = {source_id};"
+            sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE from_source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
-            sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE to_source_id = {source_id};"
+            sql_cmd = f"DELETE FROM vast_pipeline_relatedsource WHERE to_source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
 
-            sql_cmd = f"DELETE FROM vast_pipeline_association WHERE source_id = {source_id};"
+            # Delete from association
+            sql_cmd = f"DELETE FROM vast_pipeline_association WHERE source_id IN ({batch_str});"
             _run_raw_sql(sql_cmd, cursor, log=False)
-            
-            if i % 1000 == 0:
-                logger.info("Finished source id %d (%d of %d)", source_id, i, n_source_ids)
+
         t = timer.reset()
-        logger.info(f"Time to iterate over %d source ids: %f seconds", n_source_ids, t)
+        logger.info("Time to iterate over %d source ids: %.2f seconds", n_source_ids, t)
 
         # Delete source
         sql_cmd = f"DELETE FROM vast_pipeline_source WHERE run_id = {p_run_id};"
         _run_raw_sql(sql_cmd, cursor)
         
-        # Enable triggers
-        #sql_cmd = "ALTER TABLE vast_pipeline_source ENABLE TRIGGER ALL;"
-        #_run_raw_sql(sql_cmd, cursor)
-
-        # Delete comments
-        sql_cmd = f"DELETE FROM vast_pipeline_comment WHERE object_id = {p_run_id};"
-        _run_raw_sql(sql_cmd, cursor)
-
-        # Fetch image IDs associated with the pipeline run
-        sql_cmd = f"SELECT image_id FROM vast_pipeline_image_run WHERE run_id = {p_run_id};"
-        _run_raw_sql(sql_cmd, cursor)
-        image_ids = cursor.fetchall()
-
-        # Iterate over each image ID and delete related information
-        n_image_ids = len(image_ids)
-        logger.info("Iterating over %d images to delete measurements and images", n_image_ids)
-        timer.reset()
-
-        for image_id_tuple in image_ids:
-            image_id = image_id_tuple[0]
-            
-            # Check if the Image is associated with more than one run
-            sql_cmd = f"SELECT COUNT(*) FROM vast_pipeline_image_run WHERE image_id={image_id};"
-            _run_raw_sql(sql_cmd, cursor)
-            num_occurences = cursor.fetchone()[0]
-            
-            # Delete the link between the run and the image
-            sql_cmd = f"DELETE FROM vast_pipeline_image_run WHERE image_id = {image_id} AND run_id = {p_run_id};"
-            _run_raw_sql(sql_cmd, cursor)
-            
-            # If the image is associated with more than one run, do not delete the image.
-            if num_occurences > 1:
-                logger.debug("image_id %d is referenced by %d other pipeline runs, not deleting", image_id, num_occurences-1)
-                continue
-
-            try:
-                sql_cmd = f"DELETE FROM vast_pipeline_measurement WHERE image_id = {image_id};"
-                _run_raw_sql(sql_cmd, cursor)
-            except Exception as e:
-                logger.error("%s %d", e, image_id)
-                pass
-
-            try:
-                sql_cmd = f"DELETE FROM vast_pipeline_image WHERE id = {image_id};"
-                _run_raw_sql(sql_cmd, cursor)
-            except Exception as e:
-                logger.error("%s %d", e, image_id)
-                pass
         t = timer.reset()
-        logger.info("Time to iterate over %d image ids: %f seconds", n_image_ids, t)
-
-        # Fetch skyregion IDs associated with the pipeline run
-        sql_cmd = f"SELECT skyregion_id FROM vast_pipeline_skyregion_run WHERE run_id = {p_run_id};"
-        _run_raw_sql(sql_cmd, cursor, debug=True)
-        sky_ids = cursor.fetchall()
-
-        # Iterate over each skyregion ID and delete related information
-        n_sky_ids = len(sky_ids)
-        logger.info("Iterating over %d skyregion IDs to delete skyregions", n_sky_ids)
-        timer.reset()
-        for sky_id_tuple in sky_ids:
-            sky_id = sky_id_tuple[0]
-            
-            # Check if the Image is associated with more than one run
-            sql_cmd = f"SELECT COUNT(*) FROM vast_pipeline_skyregion_run WHERE skyregion_id={sky_id};"
-            _run_raw_sql(sql_cmd, cursor)
-            num_occurences = cursor.fetchone()[0]
-            
-            sql_cmd = f"DELETE FROM vast_pipeline_skyregion_run WHERE skyregion_id = {sky_id} AND run_id = {p_run_id};"
-            _run_raw_sql(sql_cmd, cursor)
-            
-            # If the skyregion is associated with more than one run, do not delete the skyregion.
-            if num_occurences > 1:
-                logger.debug("skyregion_id %d is referenced by %d other pipeline runs, not deleting", image_id, num_occurences-1)
-                continue
-            
-            try:
-                sql_cmd = f"DELETE FROM vast_pipeline_skyregion WHERE id = {sky_id};"
-                _run_raw_sql(sql_cmd, cursor)
-            except Exception as e:
-                logger.error("%s %d", e, sky_id)
-                pass
-        t = timer.reset()
-        logger.info("Time to iterate over %d sky ids: %f seconds", n_sky_ids, t)
-
-        # Finally delete the pipeline run
-        sql_cmd = f"DELETE FROM vast_pipeline_run WHERE id = {p_run_id};"
-        _run_raw_sql(sql_cmd, cursor)
+        logger.info("Time to delete source objects: %.2f seconds", t)

commit 39319841eecbd10e1176281721167591f7c0cc4d

Fix incorrect variable naming introduced in #840 (#896)
* Rename errant variable

* Updated changelog

vast_pipeline/pipeline/loading.py
@@ -198,8 +198,8 @@ def make_upload_sources(
             ids_to_delete = list(sources.values_list('id', flat=True))
             total_deleted = 0
 
-            for i in range(0, len(source_ids), batch_size):
-                batch_ids = source_ids[i : i + batch_size]
+            for i in range(0, len(ids_to_delete), batch_size):
+                batch_ids = ids_to_delete[i : i + batch_size]
                 n_deleted, details = Source.objects.filter(id__in=batch_ids).delete()
                 total_deleted += n_deleted
 